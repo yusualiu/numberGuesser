e87ef78470bab09b3dfe38d7103997731dc30a4c
Aliu Yusuf <aliu.yusuf@outlook.com>
2020-04-21 19:39:35 +0100

    Add Medium and Hard mode

--- guessNumber.py
+++ guessNumber.py
@@ -1,32 +1,51 @@
-
-def guessNumber():
-  levels = ['Easy','Medium','Hard']
+# This is a program that allows you to guess a correct number
+def guessNumber():   
+  levels = ['easy','medium','hard']
+  boundary = [1,10,20,50]
   guessess = [6,4,3]
   print('Welcome to Number Guesser game.\nThere are 3 levels; Easy,Medium and Hard.\nPlease enter the level you want to play')
   level = input("Please enter the level here: ")
-
-  if(level == levels[0]):
-    counter=0
+  # check for easy mode
+  if(level.lower() == levels[0]):    
     guessCount = guessess[0]
-    while(counter < guessCount): 
-      guessRemaining = guessCount - counter     
-      import random      
-      setNumber = random.randrange(1, 10)
-      # print(setNumber)
-      print(f'You have {guessRemaining} guess more to make')      
-      guess = int(input("Please enter your guess: "))      
-      if guess == setNumber:
-        print("You got it right!")
-        break 
-      print("That was wrong")
-      counter = counter+ 1      
-    else:
-      print('Game Over')
-
-    
-          
+    minimum = boundary[0]
+    maximum = boundary[1]
+    displayResult(guessCount,minimum,maximum)
+  elif(level.lower() == levels[1]):
+    guessCount = guessess[1]
+    minimum = boundary[0]
+    maximum = boundary[2]
+    displayResult(guessCount,minimum,maximum)
+  elif(level.lower() == levels[2]):
+    guessCount = guessess[2]
+    minimum = boundary[0]
+    maximum = boundary[3]
+    displayResult(guessCount,minimum,maximum)
+  else:
+    print('Please try again by entering level mode')
 
 
 
 
+#This displays and process game result     
+def displayResult(guessCount,minimum,maximum):
+  import random  
+  counter = 0
+  while(counter < guessCount): 
+    setNumber = random.randrange(minimum, maximum)
+    guessRemaining = guessCount - counter        
+    print(f'You have {guessRemaining} guess more to make')      
+    guess = int(input("Please enter your guess: "))      
+    if guess == setNumber:
+      print("You got it right!")
+      break 
+    print("That was wrong")
+    counter = counter+ 1      
+  else:
+    print('Game Over')
+
+
+
+
+# starter
 guessNumber()
